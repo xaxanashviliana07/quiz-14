e05e3263c010e950a28063d4953197bc56e589ec
xaxanashviliana07 <91628695+xaxanashviliana07@users.noreply.github.com>
2024-02-18 14:36:41 +0400

    Add files via upload

---
+++ vite-project/src/Questions.jsx
@@ -0,0 +1,29 @@
+export const QuizData = [
+    {
+      question: 'What is the capital of France?',
+      options: ['Berlin', 'Madrid', 'Paris', 'Rome'],
+      correctAnswer: 'Paris',
+    },
+    {
+      question: 'What is the capital of Italy?',
+      options: ['Berlin', 'Madrid', 'Paris', 'Rome'],
+      correctAnswer: 'Rome',
+    },
+    {
+      question: 'What is the capital of Russia?',
+      options: ['Berlin', 'Moscow', 'Paris', 'Rome'],
+      correctAnswer: 'Moscow',
+    },
+    {
+      question: 'What is the capital of Germany?',
+      options: ['Berlin', 'Madrid', 'Paris', 'Rome'],
+      correctAnswer: 'Berlin',
+    },
+    {
+      question: 'What is the capital of Australia?',
+      options: ['Berlin', 'Madrid', 'Canberra', 'Rome'],
+      correctAnswer: 'Canberra',
+    },
+  ];
+
+  
---
+++ vite-project/src/components/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Quiz from './Quiz'
+import '../modules/App.module.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Quiz/>
+    </>
+  )
+}
+
+export default App
